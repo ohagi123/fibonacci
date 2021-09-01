1b7759e65e503f4bc0de5e97b70f4064b8529175
ohagi123 <matsubook.daiki@gmail.com>
2021-09-02 00:22:20 +0900

    アルゴリズムを 改善

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n){
-    if (n === 0) {
-        return 0;
-    }else if (n === 1) {
-        return 1;
+    if (memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    const value = memo.get(n - 1) +memo.get(n - 2);
+    memo.set(n,value);
+    return value;
 }
 //console.log(`${process.argv[2]}番目のフィボナッチ数は${fib(process.argv[2])}です。`);
 const length = 40;
