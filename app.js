2579a9529548867e12b4334289f5d2bcb30b8d9e
ohagi123 <matsubook.daiki@gmail.com>
2021-09-01 23:39:59 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+function fib(n){
+    if (n === 0) {
+        return 0;
+    }else if (n === 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+//console.log(`${process.argv[2]}番目のフィボナッチ数は${fib(process.argv[2])}です。`);
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log(`${i}番目のフィボナッチ数は${fib(i)} です。`);
+}
